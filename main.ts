b7e954a1661d73d798d6de5165e3aac8a6416bce
Richard Knoll <riknoll@users.noreply.github.com>
2019-10-29 16:15:23 -0700

    Moving platforms

--- main.ts
+++ main.ts
@@ -16,5 +16,17 @@ const testCourse = new golf.Course(img`
     1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
 `, new golf.Point(24, 88), new golf.Point(24, 40), 3);
 
+testCourse.onLoad(function () {
+    const p = new golf.MovingPlatform(img`
+        5 4
+        b a
+    `);
+
+    p.setPath([
+        new golf.Point(screen.width - 48, 48),
+        new golf.Point(screen.width - 48, 24),
+    ], 1000);
+})
+
 const g = new golf.Game();
 g.loadCourse(testCourse);
